39e0c18ed45edbcc0e398146997d6b577881f389
Zwero <Zwero@zw.com>
2019-07-02 16:24:50 +0800

    分清前后

---
+++ node_router/index.js
@@ -0,0 +1,28 @@
+// 引入模块
+const pagesController = require('../pagesController')
+const express = require('express')
+
+const router = express.Router()
+
+// 封装路由模块
+// 判断前台页面路径
+router.get('/', pagesController.getIndexPage)
+.get('/list',pagesController.getListpage)
+.get('/detail',pagesController.getDetailPage)
+
+// 后台页面
+.get('/admin', pagesController.getAdminIndexPage)
+  .get('/admin/categories', pagesController.getCategoriesPage)
+  .get('/admin/comments', pagesController.getCommentsPage)
+  .get('/admin/login', pagesController.getloginPage)
+  .get('/admin/nav-menus', pagesController.getNen_menusPage)
+  .get('/admin/password-reset', pagesController.getPasswor_resetdPage)
+  .get('/admin/post-add', pagesController.getPost_addPage)
+  .get('/admin/posts', pagesController.getPostsPage)
+  .get('/admin/profile', pagesController.getProfilePage)
+  .get('/admin/settings', pagesController.getSettingsPage)
+  .get('/admin/slides', pagesController.getSlidesPage)
+  .get('/admin/users', pagesController.getUsersPage)
+
+
+module.exports = router
