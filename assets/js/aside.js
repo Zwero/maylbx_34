8925a7f50dfe40413071bf82018b2cc1340f80c6
Zwero <Zwero@zw.com>
2019-07-05 15:59:03 +0800

    爱

--- assets/js/aside.js
+++ assets/js/aside.js
@@ -23,27 +23,3 @@ $(function () {
   $('li').removeClass('active')
   $('#' + routername).addClass("active")
 })
-
-
-// // 获取你想操作的dom元素
-// var menu_posts = $('#menu-posts')
-// // 判断路由名称
-// // 如果是：post-add   |   posts   |   categories,就 要为id="menu-posts"来设置添加对应的样式和设置相应的属性
-// if (routername == 'post-add' || routername == 'posts' || routername == 'categories') {
-//   menu_posts.addClass('in')
-//   menu_posts.attr('aria-expanded', true)
-// }
-
-// // 设置菜单也需要这个处理
-// var menu_settings = $('#menu-settings')
-// // 判断路由名称
-// // 如果是：post-add   |   posts   |   categories,就 要为id="menu-posts"来设置添加对应的样式和设置相应的属性
-// if (routername == 'nav-menus' || routername == 'slides' || routername == 'settings') {
-//   menu_settings.addClass('in')
-//   menu_settings.attr('aria-expanded', true)
-// }
-
-// // 添加active样式：排它法
-// $('li').removeClass('active')
-// $('#' + routername).addClass('active')
-// })
