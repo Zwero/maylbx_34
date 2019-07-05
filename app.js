ed877aaca2ae7f6725a95e12a1f4b447bcbed16c
Zwero <Zwero@zw.com>
2019-07-05 15:57:09 +0800

    更改端口号

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const ejs = require('ejs')
 // 2. 创建对象
 const app = express()
 // 3. 监视对象
-app.listen(3000,()=>{
-console.log('http://127.0.0.1:3000');
+app.listen(3006,()=>{
+console.log('http://127.0.0.1:3006');
 // 引擎 
 app.set('view engine', 'ejs')
 app.set('/views','views')
