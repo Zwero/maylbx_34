14615cfeaf0196784e7c7f07fb79373f51115f16
Zwero <Zwero@zw.com>
2019-07-07 16:55:41 +0800

    同步文本框

--- assets/js/postsAdd.js
+++ assets/js/postsAdd.js
@@ -1,7 +1,7 @@
 $(function () {
-  // 1. 加载所有分类数据
+ // 添加的所有分类
   $.ajax({
-    url:'/getPostsScreen',
+    url: '/getPostsScreen',
     dataType: 'json',
     success: function (res) {
       // 生成分类数据的动态结构
@@ -11,5 +11,23 @@ $(function () {
       }
       $('#category').html(html)
     }
+  })
+
+  // 生成副文本框 覆盖文本框的ID
+  CKEDITOR.replace('content')
+// 给按钮添加点击事件
+  $('.btnSave').on('click', function(a){
+    a.preventDefault()
+    // 同步数据
+    CKEDITOR.instances.content.updateElement()
+    $.ajax({
+      type:'post',
+      url:'/addpost',
+      data:$('row').serialize(),
+      dataType:'json',
+      success:function(res){
+        
+      }
+    })
   })
 })
